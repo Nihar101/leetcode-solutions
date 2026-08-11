99a2decb8a893cbb13f074d7e0844ccda11d873f
Nihar Ranjan Sahu <ranjusahua2018@gmail.com>
2026-08-11 12:56:16 +0530

    Time: 945 ms (34.37%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,16 +1,6 @@
 # Write your MySQL query statement below
 select p.product_id, (coalesce(round(
-    sum(case
-
-        when p.price is not null and u.units is not null then p.price*u.units
-        else 0
-        end
-        )/sum(
-            case
-        when p.price is not null and u.units is not null then u.units
-        else 0
-        end
-        ),2),0)
+    sum(p.price*u.units)/sum(u.units),2),0)
 ) as average_price from Prices as p
 left join UnitsSold as u
  on p.product_id = u.product_id 
